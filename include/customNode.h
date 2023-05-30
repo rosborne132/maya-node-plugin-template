617d83050e19eeebbb88477de7e360cb30c0bf30
osbornr <ozborne132@gmail.com>
2023-05-30 06:58:53 -0700

    update template code

--- include/customNode.h
+++ include/customNode.h
@@ -1,26 +1,16 @@
-#ifndef CUSTOM_NODE_H
-#define CUSTOM_NODE_H
+#pragma once
 
 #include <maya/MPxNode.h>
 #include <maya/MTypeId.h>
 
 class CustomNode : public MPxNode {
 public:
-  virtual MStatus compute(const MPlug &plug, MDataBlock &dataBlock);
-
-  static void *creator() { return new CustomNode(); };
-
-  static MStatus initialize();
-
   // The node id
   static MTypeId typeId;
-
-  static MObject inputVal;
-
-  static MObject inputCurve;
-
   // Output attribute holding the computed data
   static MObject outputVal;
-};
 
-#endif
+  virtual MStatus compute(const MPlug &plug, MDataBlock &dataBlock);
+  static void *creator() { return new CustomNode(); };
+  static MStatus initialize();
+};
--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,19 @@
 #include "customNode.h"
 
 MStatus initializePlugin(MObject obj) {
-  MFnPlugin fnPlugin(obj, "Autodesk", "1.0", "Any");
-  MStatus status = fnPlugin.registerNode(
+  MStatus status;
+  MFnPlugin fnPlugin(obj, "Autodesk", "1.0", "Any", &status);
+  CHECK_MSTATUS_AND_RETURN_IT(status);
+  CHECK_MSTATUS_AND_RETURN_IT(fnPlugin.registerNode(
       "CustomNode", CustomNode::typeId, CustomNode::creator,
-      CustomNode::initialize, MPxNode::kDependNode);
-
-  if (status != MS::kSuccess)
-    status.perror("Could not register the poseReader node");
+      CustomNode::initialize, MPxNode::kDependNode));
 
   return status;
 }
 
 MStatus uninitializePlugin(MObject obj) {
-  MFnPlugin fnPlugin;
-  fnPlugin.deregisterNode(CustomNode::typeId);
+  MFnPlugin fnPlugin(obj);
+  CHECK_MSTATUS_AND_RETURN_IT(fnPlugin.deregisterNode(CustomNode::typeId));
 
   return MS::kSuccess;
 }
